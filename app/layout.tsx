f783a463bbe2a91c66f446dad8f4fa013ecca314
mnzrva-lab <mnzrva@gmail.com>
2026-08-22 11:19:54 +0200

    Load compact Admin refinement styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ import './v12-content-pass.css'
 import './v12-fixes.css'
 import './course-offering-followup.css'
 import './compact-refinement.css'
+import './admin-compact-pass.css'
 
 export const metadata: Metadata = {
   title: 'Classics Courses with Timothy Lowenhaupt',
