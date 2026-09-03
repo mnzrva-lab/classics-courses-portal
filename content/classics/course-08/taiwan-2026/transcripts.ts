7a039d3186185881879d30ab2a5933733a74d50a
mnzrva-lab <mnzrva@gmail.com>
2026-09-03 20:46:38 +0200

    Migrate Course 8 Class 2 transcript

--- content/classics/course-08/taiwan-2026/transcripts.ts
+++ content/classics/course-08/taiwan-2026/transcripts.ts
@@ -3,6 +3,10 @@ import class1Part2 from './class-1/transcript-part-2.json'
 import class1Part3 from './class-1/transcript-part-3.json'
 import class1Part4 from './class-1/transcript-part-4.json'
 import class1Part5 from './class-1/transcript-part-5.json'
+import class2Part1 from './class-2/transcript-part-1.json'
+import class2Part2 from './class-2/transcript-part-2.json'
+import class2Part3 from './class-2/transcript-part-3.json'
+import class2Part4 from './class-2/transcript-part-4.json'
 import med1Part1 from './meditation-1/transcript-part-1.json'
 import med1Part2 from './meditation-1/transcript-part-2.json'
 import med1Part3 from './meditation-1/transcript-part-3.json'
@@ -30,6 +34,12 @@ export const course8Transcripts: Record<string, Course8TranscriptChapter[]> = {
     ...class1Part3.chapters,
     ...class1Part4.chapters,
     ...class1Part5.chapters,
+  ],
+  class2: [
+    ...class2Part1.chapters,
+    ...class2Part2.chapters,
+    ...class2Part3.chapters,
+    ...class2Part4.chapters,
   ],
   med1: [
     ...med1Part1.chapters,
