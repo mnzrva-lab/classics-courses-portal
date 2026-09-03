2a371a44425469af2d6b051c428d4017bafe2984
mnzrva-lab <mnzrva@gmail.com>
2026-09-03 19:50:49 +0200

    Connect Meditation 1 transcript to Course 8 loader

--- content/classics/course-08/taiwan-2026/transcripts.ts
+++ content/classics/course-08/taiwan-2026/transcripts.ts
@@ -3,6 +3,9 @@ import class1Part2 from './class-1/transcript-part-2.json'
 import class1Part3 from './class-1/transcript-part-3.json'
 import class1Part4 from './class-1/transcript-part-4.json'
 import class1Part5 from './class-1/transcript-part-5.json'
+import med1Part1 from './meditation-1/transcript-part-1.json'
+import med1Part2 from './meditation-1/transcript-part-2.json'
+import med1Part3 from './meditation-1/transcript-part-3.json'
 import med2Part1 from './meditation-2/transcript-part-1.json'
 import med2Part2 from './meditation-2/transcript-part-2.json'
 import med2Part3 from './meditation-2/transcript-part-3.json'
@@ -27,6 +30,11 @@ export const course8Transcripts: Record<string, Course8TranscriptChapter[]> = {
     ...class1Part3.chapters,
     ...class1Part4.chapters,
     ...class1Part5.chapters,
+  ],
+  med1: [
+    ...med1Part1.chapters,
+    ...med1Part2.chapters,
+    ...med1Part3.chapters,
   ],
   med2: [
     ...med2Part1.chapters,
